31a1d27c7251473d700401c1e44346e34e71e5d6
Jeehee77 <42367472+Jeehee77@users.noreply.github.com>
2023-03-22 16:53:59 +0900

    Update H－Index.js

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -4,7 +4,6 @@ function solution(citations) {
         
         - citation를 내림차순으로 정렬한다.
         - citation[i] > h_index라면 h-index에 1을 더한다.
-        - 내림차순으로 정렬하여 큰 수부터 검사하므로 처음 h-index가 발견되면 그 수가 최대값이다.
     */
     let h_index = 0;
     citations.sort((a, b) => b - a);
